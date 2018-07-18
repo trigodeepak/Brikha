0072a73a039e4a83ba659eea8244c1f260e2c930
trigodeepak <trigodeepak@gmail.com>
2018-07-19 01:42:04 +0530

    Made info activity

--- app/src/main/java/net/brikha/Activity/Info_activity.java
+++ app/src/main/java/net/brikha/Activity/Info_activity.java
@@ -21,7 +21,7 @@ public class Info_activity extends AppCompatActivity {
         dev_info.setText(Html.fromHtml(value));
         dev_info.setMovementMethod(LinkMovementMethod.getInstance());
 
-        value = "<html>This app shows baby name and their meaning.\n <a href=\"https://brikha.net/\">Visit our Website</a></html>";
+        value = "<html>Brikha is the first project of its kind, to create an application and website, that's dedicated for our Assyrian baby names. Each name is presented with meaning, pronunciation and more. The project is a result of collaboration between Ninos and Sizar Hozaya. We welcome any new additions, send the name details to <a href=\"mailto:admin@brikha.net\">admin email</a> and we will include them. Our website <a href=\"https://brikha.net/\">www.brikha.net</a></html>";
         TextView text = (TextView) findViewById(R.id.info_text);
         text.setText(Html.fromHtml(value));
         text.setMovementMethod(LinkMovementMethod.getInstance());
